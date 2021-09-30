92fe26a6e3bff4a72c75c0ac765f2aff741fde3c
Alamin934 <islamalamin934@gmail.com>
2021-09-30 18:53:55 +0600

    Update active link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div className="flex justify-center gap-4 mb-8">
             <div>
-                <Link className="bg-red-700 px-4 py-2 text-white font-semibold rounded border cursor-pointer duration-300  hover:bg-white hover:border hover:border-red-700 hover:text-red-700" to="/home">Home</Link>
+                <NavLink
+                    activeStyle={{
+                        backgroundColor: "white",
+                        color: "rgb(185, 28, 28)",
+                        borderColor: "rgb(185, 28, 28)"
+                    }}
+                    className="bg-red-700 px-4 py-2 text-white font-semibold rounded border cursor-pointer duration-300  hover:bg-white hover:border hover:border-red-700 hover:text-red-700" to="/home"
+                >Home</NavLink>
             </div>
             <div>
-                <Link className="bg-red-700 px-4 py-2 text-white font-semibold rounded border cursor-pointer duration-300  hover:bg-white hover:border hover:border-red-700 hover:text-red-700" to="/resturent">Resturent</Link>
+                <NavLink
+                    activeStyle={{
+                        backgroundColor: "white",
+                        color: "rgb(185, 28, 28)",
+                        borderColor: "rgb(185, 28, 28)"
+                    }}
+                    className="bg-red-700 px-4 py-2 text-white font-semibold rounded border cursor-pointer duration-300  hover:bg-white hover:border hover:border-red-700 hover:text-red-700" to="/resturent"
+                >Resturent</NavLink>
             </div>
         </div>
     );
